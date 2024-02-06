c4212b88b8144f250d5dffebb27a41f9e659c1ac
Alix Azé <alix.az10@gmail.com>
2024-02-06 11:11:50 +0100

    New class Channel

---
+++ src/main/java/com/example/demo/back/DAO/ChannelRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.back.DAO;
+
+import com.example.demo.back.model.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChannelRepository extends JpaRepository<Channel, Integer> {
+}
