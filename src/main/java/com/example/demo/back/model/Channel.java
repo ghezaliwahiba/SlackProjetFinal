df73b065483f17a00a3e5410ef2e82c147c6d022
Alix Azé <alix.az10@gmail.com>
2024-02-06 16:18:33 +0100

    fin Chanel Class

--- src/main/java/com/example/demo/back/model/Channel.java
+++ src/main/java/com/example/demo/back/model/Channel.java
@@ -12,18 +12,66 @@ public class Channel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String channelName;
-//    @ManyToMany
-//    private List<User> = new ArrayList;
 
-//    @ManyToMany
-//    @JoinTable(
-//            name = "messages_channel",
-//            joinColumns = @JoinColumn(name = "channel_id"),
-//            inverseJoinColumns = @JoinColumn(name = "message_id")
-//    )
-//    private List<Message> = new ArrayList;
+    @ManyToMany
+    private List<User> users = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(
+            name = "messages_channel",
+            joinColumns = @JoinColumn(name = "channel_id"),
+            inverseJoinColumns = @JoinColumn(name = "message_id")
+    )
+    private List<Message> messages = new ArrayList<>();
 
+    public Channel() {
+    }
 
+    public Channel(String channelName, List<User> users, List<Message> messages) {
+        this.channelName = channelName;
+        this.users = users;
+        this.messages = messages;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "id=" + id +
+                ", channelName='" + channelName + '\'' +
+                ", users=" + users +
+                ", messages=" + messages +
+                '}';
+    }
 }
