bee5133a6dbe7c8a8bf0fd838037b1e7b8511d50
ghezaliwahiba <ghezaliwahiba@yahoo.fr>
2024-02-06 11:43:04 +0100

    LocalDateTime

--- src/main/java/com/example/demo/back/controller/MessageContoller.java
+++ src/main/java/com/example/demo/back/controller/MessageContoller.java
@@ -1,2 +1,52 @@
-package com.example.demo.back.controller;public class MessageContoller {
+package com.example.demo.back.controller;
+import com.example.demo.back.model.Message;
+import com.example.demo.back.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@RestController
+public class MessageContoller {
+@Autowired
+    MessageService messageService;
+    @GetMapping("messages")
+    public List<Message> findAll(){
+        return messageService.getAll();
+    }
+
+/*
+    @GetMapping("messages")
+    public List<MessageDTO> findAll(){
+        List<MessageDTO> dtos = new ArrayList<>();
+        for(Message entity :messageService.getAll())
+            dtos.add(MessageMapper.convertToDTO(entity));
+        return dtos;
+    }
+ */
+    @PostMapping("messages")
+    public void addMessage(@RequestBody Message message) {
+            messageService.add(message);
+            LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+
+    /*
+    @PostMapping("messages")
+    public void addMessage(@RequestBody MessageDTO message) {
+        messageService.add(message);
+    }
+
+     */
+
+    @GetMapping("messages/{id}")
+    public ResponseEntity<Message> findById(@PathVariable("id") Integer id){
+        Optional<Message> opt = messageService.findById(id);
+        if(opt.isEmpty())
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(opt.get());
+    }
+
 }
--- src/main/java/com/example/demo/back/model/Message.java
+++ src/main/java/com/example/demo/back/model/Message.java
@@ -1,19 +1,39 @@
-package com.example.demo.back;
+package com.example.demo.back.model;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import java.time.LocalDate;
-
+@Entity
+@Table(name="messages")
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private LocalDate date;
+
+    private LocalDateTime date;
     private String content;
 
-
-
-
+    public Message() {
+    }
+    public Message(LocalDateTime date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
