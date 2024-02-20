0bee86fc3104fae5dde58f8311f5ec7f2601f394
Sukjin <osj.informatique@gmail.com>
2024-02-20 22:47:44 +0100

    Creation des composants users/users-add, -edit, -list et -delete

---
+++ src/front_angular/src/app/components/users/users-delete/users-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-delete',
+  templateUrl: './users-delete.component.html',
+  styleUrl: './users-delete.component.css'
+})
+export class UsersDeleteComponent {
+
+}
