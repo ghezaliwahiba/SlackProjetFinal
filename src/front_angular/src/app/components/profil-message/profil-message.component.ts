83da2b275f9da42aae2e91f0f1773c4f50027439
LuciaBOURQUE <80245094+LuciaBOURQUE@users.noreply.github.com>
2024-02-22 07:28:23 +0100

    add services directory and generate files with angular

---
+++ src/front_angular/src/app/components/profil-message/profil-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-message',
+  templateUrl: './profil-message.component.html',
+  styleUrl: './profil-message.component.css'
+})
+export class ProfilMessageComponent {
+
+}
