e84b6ca75194518c30f03e1cf46d89012847e927
ghezaliwahiba <ghezaliwahiba@yahoo.fr>
2024-02-23 18:38:30 +0100

    style

--- src/front_angular/src/app/app-routing.module.ts
+++ src/front_angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChannelComponent } from './core/sidebar/channel/channel.component';
 import { UserComponent } from './core/sidebar/user/user.component';
+import { ListMessagesComponent } from './core/chat/list-messages/list-messages.component';
 
 const routes: Routes = [
   { path: 'channels', component: ChannelComponent },
   { path: 'users', component: UserComponent },
+  { path: 'messages', component: ListMessagesComponent },
 ];
 
 @NgModule({
