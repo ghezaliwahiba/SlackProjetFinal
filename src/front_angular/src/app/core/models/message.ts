c48b4a7c7b137884e21ace241db51a0c0605616e
Alix Azé <alix.az10@gmail.com>
2024-02-23 11:43:22 +0100

    ajout message service et model message

--- src/front_angular/src/app/core/models/message.ts
+++ src/front_angular/src/app/core/models/message.ts
@@ -1,3 +1,13 @@
+import { Time } from "@angular/common";
+import { User } from "./user";
+import { Channel } from "./channel";
+
 export interface Message {
+    id: number;
+    localDate: Date;
+    localTime: Time;
+    content: String;
+    user: User;
+    channel: Channel;
 // à remplir
 }
--- src/front_angular/src/app/service/messages.service/messages.service.ts
+++ src/front_angular/src/app/service/messages.service/messages.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Message } from '../../core/models/message';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
+  apiUrl: String = 'http://localhost:8080/messages';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllMessages(): Observable<Message[]> {
+    return this.http.get<Message[]>(`${this.apiUrl}`);
+  }
+  addMessage(message: Message): Observable<Message> {
+    return this.http.post<Message>(`${this.apiUrl}`, message);
+  }
+
+  deleteMessage(id: number): Observable<Message> {
+    return this.http.delete<Message>(`${this.apiUrl}/${id}`);
+  }
+
+  getMessagesById(id: number): Observable<Message> {
+    return this.http.get<Message>(`${this.apiUrl}/${id}`);
+  }
+
+  updateMessage(message: Message): Observable<Message> {
+    return this.http.put<Message>(`${this.apiUrl}/${message.id}`, message);
+  }
 }
